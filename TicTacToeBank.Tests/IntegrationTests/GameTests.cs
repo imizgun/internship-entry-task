f916c9f7d57d4a5f1d26f4880de4d99bf26d5b1a
Mikhail Marasanov <mihamar2005@gmail.com>
2025-07-21 16:30:48 +0300

    format & readme & integration test

---
+++ TicTacToeBank.Tests/IntegrationTests/GameTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Text.Json;
+using TicTacToeBank.Application.DTOs;
+using TicTacToeBank.Core.Domain;
+using TicTacToeBank.Core.Domain.Enums;
+using TicTacToeBank.Responses;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TicTacToeBank.Tests.IntegrationTest;
+
+public class GameTests : BaseIntegrationTest {
+	public GameTests(IntegrationTestWebFactory factory, ITestOutputHelper output) : base(factory, output) { }
+	
+	[Fact]
+	public async Task CreateGame_ShouldReturnCreatedGame() {
+		// Arrange
+		var xPlayerId = Guid.NewGuid();
+		var oPlayerId = Guid.NewGuid();
+
+		var requestContent = new StringContent(
+			JsonSerializer.Serialize(new CreateGameDto{
+				XPlayerId = xPlayerId,
+				OPlayerId = oPlayerId
+			}), Encoding.UTF8, "application/json");
+
+		// Act
+		var response = await Client.PostAsync("/api/games", requestContent);
+
+		// Assert
+		var responseBody = await response.Content.ReadAsStringAsync();
+		Output.WriteLine(responseBody);
+		Output.WriteLine(response.StatusCode.ToString());
+		Assert.True(response.IsSuccessStatusCode);
+		var createdGame = JsonSerializer.Deserialize<CreateGameResponse>(responseBody, JsonSerializerOptions);
+
+		Assert.NotNull(createdGame);
+		Assert.Equal(xPlayerId, createdGame.XPlayerId);
+		Assert.Equal(oPlayerId, createdGame.OPlayerId);
+		Assert.NotEqual(Guid.Empty, createdGame.Id);
+	}
+}
---
+++ TicTacToeBank.Tests/IntegrationTests/IntegrationTestWebFactory.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.PostgreSql;
+using TicTacToeBank.DatabaseAccess;
+using Xunit;
+
+namespace TicTacToeBank.Tests.IntegrationTest;
+
+public class IntegrationTestWebFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+	private PostgreSqlContainer? _dbContainer;
+
+	protected override void ConfigureWebHost(IWebHostBuilder builder)
+	{
+		builder.ConfigureAppConfiguration((context, config) =>
+		{
+			config.AddJsonFile("appsettings.json");
+		});
+
+		builder.ConfigureTestServices(services =>
+		{
+			var configuration = services.BuildServiceProvider().GetRequiredService<IConfiguration>();
+
+			var dbName = configuration["Postgres:Database"] ?? "tictactoe";
+			var dbUser = configuration["Postgres:Username"] ?? "postgres";
+			var dbPass = configuration["Postgres:Password"] ?? "123";
+
+			_dbContainer = new PostgreSqlBuilder()
+				.WithImage("postgres:latest")
+				.WithDatabase(dbName)
+				.WithUsername(dbUser)
+				.WithPassword(dbPass)
+				.Build();
+
+			_dbContainer.StartAsync().GetAwaiter().GetResult();
+
+			var connectionString = _dbContainer.GetConnectionString();
+
+			var descriptor = services.SingleOrDefault(
+				d => d.ServiceType == typeof(DbContextOptions<TicTacToeDbContext>));
+
+			if (descriptor is not null)
+				services.Remove(descriptor);
+
+			services.AddDbContext<TicTacToeDbContext>(options =>
+			{
+				options.UseNpgsql(connectionString);
+			});
+		});
+	}
+
+	public Task InitializeAsync() {
+		return _dbContainer?.StartAsync() ?? Task.CompletedTask;
+	}
+	
+	public new Task DisposeAsync() {
+		return _dbContainer?.StopAsync() ?? Task.CompletedTask;
+	}
+}
